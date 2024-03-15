10a2a82c6a6ea1ea148f17d22b0515b7407c6d21
Greentaylor27 <7196@holbertonstudents.com>
2024-03-15 15:43:44 +0000

    Fixing pep8'

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,6 +3,8 @@
 import requests
 import sys
 
+
+
 class GetMethod:
 
     def to_do(employ_id):
@@ -17,20 +19,22 @@ class GetMethod:
         # Grabbing employee name and ID
         User = requests.get(Users_url)
         Employee_name = User.json().get('name')
-        
-        #Setting up to do
+
+
+        # Setting up to do
         params = {"userId": employ_id}
         todos_total = requests.get(To_do_URL, params=params)
         todos = todos_total.json()
         finished_tasks = [todo for todo in todos if todo['completed']]
 
-        #Formatting
+        # Formatting
         print(
             f"Employee {Employee_name} is done with tasks"
             f"({len(finished_tasks)}/{len(todos)}):"
         )
         for task in finished_tasks:
             print(f"\t{task['title']}")
+
 
 if __name__ == "__main__":
     args = int(sys.argv[1])
