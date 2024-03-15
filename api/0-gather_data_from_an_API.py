84b18291ffc16d40917e99c59e985184459a536f
Greentaylor27 <7196@holbertonstudents.com>
2024-03-15 14:34:08 +0000

    Adding comments

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,19 +6,21 @@ class GetMethod:
 
     def to_do(employ_id):
 
-
+        # Setting URLS
         Base_URL = "https://jsonplaceholder.typicode.com"
         Users_URL =  requests.get(Base_URL + "/users")
         To_do_URL = requests.get(Base_URL + "/todo")
 
-
+        # Setting up JSONS
         User_json = Users_URL.json()
         To_Do_json = To_do_URL.json()
 
-
+        #Pulling name and ID from User_json
         for item in User_json:
             name = item['name']
             id = item['id']
+
+        # Testing
         if User_json[id] == To_Do_json[id]:
             print("Well done")
 
