5cbaa9fdbc31a17cd73752ee1fcda312d4eff733
Greentaylor27 <7196@holbertonstudents.com>
2024-03-19 23:49:47 +0000

    Pushing something to get a grade

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module to GET data from a json"""
+import requests
+import sys
+import csv
+import json
+
+
+class GetMethod:
+
+    def to_do(employ_id):
+
+        employ_id = sys.argv[1]
+
+        # Setting URLS
+        Base_URL = "https://jsonplaceholder.typicode.com"
+        Users_url = f"{Base_URL}/users/{employ_id}"
+        To_do_URL = f"{Base_URL}/todos"
+
+        # Grabbing employee name and ID
+        User = requests.get(Users_url).json
+        username = User.get('username')
+
+        # Setting up to do
+        params = {"userId": employ_id}
+        todos_total = requests.get(To_do_URL, params=params)
+        todos = todos_total.json()
+
+        data = [{
+            'task': todo['title'],
+            'completed': todo['completed'],
+            'username': username
+        } for todo in todos]
+
+        # Formatting
+        formatted_task = {str(employ_id): data}
+
+        file_name = f'{employ_id}.json'
+        with open(file_name, 'w') as file:
+            json.dump(formatted_task, file)
+        print(f'Data for employee_id {employ_id} written to {file_name}.')
+
+
+if __name__ == "__main__":
+
+    GetMethod.to_do(int(sys.argv[1]))
