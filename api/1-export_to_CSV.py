0f1c85c7308bd26f8565d86c825d965bcd579783
Greentaylor27 <7196@holbertonstudents.com>
2024-03-20 00:05:24 +0000

    Rewritting whole file to fix something

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,34 +9,28 @@ import sys
 class GetMethod:
 
     def to_do(employ_id):
+        api_url = "https://jsonplaceholder.typicode.com"
+        users_url = f"{api_url}/users/{employ_id}"
+        todos_url = f"{api_url}/todos"
+        user_data = requests.get(users_url).json()
+        username = user_data.get('username')
 
-        # Setting URLS
-        Base_URL = "https://jsonplaceholder.typicode.com"
-        Users_url = f"{Base_URL}/users/{employ_id}"
-        To_do_URL = f"{Base_URL}/todos"
-
-        # Grabbing employee name and ID
-        User = requests.get(Users_url).json()
-        username = User.get('username')
-
-        # Setting up to do
-        params = {"userId": employ_id}
-        todos_total = requests.get(To_do_URL, params=params)
-        todos = todos_total.json()
+        params = {'userId': employ_id}
+        todos_response = requests.get(todos_url, params=params)
+        todos = todos_response.json()
 
         data = [{
-            'task': todo['title'],
-            'completed': todo['completed'],
-            'username': username
+            "task": todo['title'],
+            "completed": todo['completed'],
+            "username": username
         } for todo in todos]
 
-        # Formatting
         formatted_task = {str(employ_id): data}
 
-        file_name = f'{employ_id}.json'
+        file_name = f"{employ_id}.json"
         with open(file_name, 'w') as file:
             json.dump(formatted_task, file)
-        print(f'Data for employee_id {employ_id} written to {file_name}.')
+        print(f"Data for employee_id {employ_id} witten to {file_name}.")
 
 
 if __name__ == "__main__":
