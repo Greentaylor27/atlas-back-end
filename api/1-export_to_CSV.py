8ca8b9ec638188fc36036ed807bc036a9ccc5f99
Greentaylor27 <7196@holbertonstudents.com>
2024-03-20 00:14:37 +0000

    PLEASE

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,33 +6,23 @@ import requests
 import sys
 
 
-class GetMethod:
+def to_do(employ_id):
+    api_url = "https://jsonplaceholder.typicode.com"
+    users_url = f"{api_url}/users/{employ_id}"
+    todos_url = f"{api_url}/todos"
+    user_data = requests.get(users_url).json()
+    username = user_data.get('username')
 
-    def to_do(employ_id):
-        api_url = "https://jsonplaceholder.typicode.com"
-        users_url = f"{api_url}/users/{employ_id}"
-        todos_url = f"{api_url}/todos"
-        user_data = requests.get(users_url).json()
-        username = user_data.get('username')
+    todos_response = requests.get(todos_url, params={'userId': employ_id}).json()
 
-        params = {'userId': employ_id}
-        todos_response = requests.get(todos_url, params=params)
-        todos = todos_response.json()
-
-        data = [{
-            "task": todo['title'],
-            "completed": todo['completed'],
-            "username": username
-        } for todo in todos]
-
-        formatted_task = {str(employ_id): data}
-
-        file_name = f"{employ_id}.json"
-        with open(file_name, 'w') as file:
-            json.dump(formatted_task, file)
-        print(f"Data for employee_id {employ_id} witten to {file_name}.")
+    file_name = f"{employ_id}.csv"
+    with open(file_name, 'w') as file:
+        write = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for todo in todos_response:
+            write.writerow([employ_id, username, 
+                            todo['completed'], todo['title']])
 
 
 if __name__ == "__main__":
 
-    GetMethod.to_do(int(sys.argv[1]))
+    to_do(int(sys.argv[1]))
