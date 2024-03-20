aa18321e1fd5402fbe4f0d715bb06b9c8a8711aa
Greentaylor27 <7196@holbertonstudents.com>
2024-03-20 00:17:57 +0000

    AT THIS POINT I NEED MENTAL ASSISSTANCE

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -14,7 +14,7 @@ def to_do(employ_id):
     username = user_data.get('username')
 
     todos_response = requests.get(todos_url,
-                                params={'userId': employ_id}).json()
+                                  params={'userId': employ_id}).json()
 
     file_name = f"{employ_id}.csv"
     with open(file_name, 'w') as file:
